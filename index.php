a673dd18c44b645beae6e48bae0f4a9d149741d9
Pareekshith A Abhimanyu Raja <paa36@afsconnect2.njit.edu>
2017-10-04 23:14:00 -0400

    Made changes to php file

--- index.php
+++ index.php
@@ -14,9 +14,12 @@ echo '<br>'.'<hr>';
 
 $string = "Hello Class";
 
+echo 'Replace "-" with "/" in $date'. '<br>' .'<br>';
 $date = str_replace('-','/',$date);
 echo "$date"."<br>"."<hr>";
 
+
+echo 'Compare $date with $tar'. '<br>' .'<br>';
 if (strcmp($date,$tar) > 0)
 echo "The future". "<br>"."<hr>";
 elseif(strcmp($date,$tar) < 0)
@@ -24,6 +27,7 @@ elseif(strcmp($date,$tar) < 0)
 else
   echo "Oops". "<br>"."<hr>";
  
+echo 'Search for "/" in $date and print out all positions'. '<br>' .'<br>';
 $position1 = strpos ($date, '/', 0);
 echo $position1 ." ";
 $i=1;
@@ -38,25 +42,31 @@ while ($i>0)
   }
 }   
 
+echo 'Count the number of words in $date'. '<br>' .'<br>';
 echo str_word_count($date)."<br>"."<hr>";
 
+echo 'Return the length of a string'. '<br>' .'<br>';
 echo strlen($string)."<br>"."<hr>";
 
+echo 'Return the ASCII value of the first character of a string'. '<br>' .'<br>';
 echo ord($string)."<br>"."<hr>";
  
+ echo 'Return the last two characters in $date'. '<br>' .'<br>';
 echo substr($date, -2) ."<br>"."<hr>";
 
-
+echo 'Break $date into any array and set separator parameter as "/" and print'. '<br>' .'<br>';
 $dateArr = explode('/', $date);
 $countArr = count ($dateArr);
 
 for ($i = 0; $i < $countArr; $i++) 
     {
         print_r("$dateArr[$i]" . " ");
-        echo "<br>"."<hr>";
+        
       }
+echo "<br>"."<hr>";
 
 
+echo 'Loop through $year and identify whether each year is a loop year'. '<br>' .'<br>';
 foreach ($year as $value) 
      {
         switch ($value % 4) 
@@ -76,7 +86,7 @@ foreach ($year as $value)
 	    echo 'false' . ' ';
         }
       }
-echo "<br>"."<hr>";
+echo "<br>";
 
 $countArr = count($year);
       for ($i = 0; $i < $countArr; $i++) 
